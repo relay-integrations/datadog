c253f15348997bc9ef8f16ea091f5d26f53bad5e
Eric Sorenson <eric@puppet.com>
2020-08-04 18:05:01 -0700

    initial cut at event post step

---
+++ steps/event-post/datadog-event-post.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# posts an event to datadog from relay
+
+from datadog import initialize, api
+from relay_sdk import Interface, Dynamic as D
+
+relay = Interface()
+
+options = {
+  'api_key': relay.get(D.datadog.connection.api_key),
+  'app_key': relay.get(D.datadog.connection.app_key)
+}
+
+initialize(**options)
+
+event_payload = {
+  source_type_name: "puppet",
+  title: relay.get(D.event_title),
+  text: relay_get(D.event_text),
+  alert_type: relay_get(D.event_type)
+}
+
+api.Event.create(event_payload)
+
+print('Emitted event to Datadog API: {}'.format(event_payload))
