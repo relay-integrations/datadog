4b1388465c9550d25132dc6a8bb45591450d275e
Kenaz <kenaz@puppetlabs.com>
2020-12-16 15:18:11 -0800

    Dumping webhook payload

--- triggers/event-fired/server.py
+++ triggers/event-fired/server.py
@@ -1,12 +1,18 @@
 from relay_sdk import Interface, WebhookServer
 from quart import Quart, request
+import logging
+import json
 
 relay = Interface()
 app = Quart('event-fired')
 
+logging.getLogger().setLevel(logging.INFO)
+
 @app.route('/', methods=['POST'])
 async def webhook():
     data = await request.get_json()
+
+    logging.info("Received the following webhook payload: \n%s", json.dumps(data, indent=4))
 
     for key, value in request.headers.items():
         if key.startswith("X-Datadog-"):
